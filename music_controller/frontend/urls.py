a03272c9cb6daacf5b7720da9e932271ca430ead
sushantdesaur <sushantdesaur@live.in>
2023-12-21 20:21:53 +0530

    Added create and join  url to the backend urls

--- music_controller/frontend/urls.py
+++ music_controller/frontend/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index),
+    path('join', index),
+    path('create', index)
 ]
