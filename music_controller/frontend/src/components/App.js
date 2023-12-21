9859942f26d71abd2c20f37f8ae4f1f66f10dd59
sushantdesaur <sushantdesaur@live.in>
2023-12-21 20:18:32 +0530

    Added react router v6 to the project

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+import { BrowserRouter } from "react-router-dom";
 
 export default class App extends Component {
     constructor(props) {
         super(props);
     }
     render() {
-        return <h1>Testing React Code</h1>;
+        return (
+        <HomePage/>
+        );
     }
 }
 
 const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  appDiv
+);
---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import { Routes, Route } from "react-router-dom";
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render () {
+        return (
+            <Routes>
+              <Route path='/' element={<p>This is the home page</p>} />
+              <Route path='/join' element={<RoomJoinPage />} />
+              <Route path='/create' element={<CreateRoomPage />} />
+            </Routes>
+        ) 
+    }
+}
