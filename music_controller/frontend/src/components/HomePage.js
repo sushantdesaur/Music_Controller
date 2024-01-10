b83fe36abd59b9777e2a37116276db2a50821ecb
sushantdesaur <sushantdesaur@live.in>
2024-01-10 23:21:42 +0530

    Made leave room functionality

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -11,6 +11,7 @@ export default class HomePage extends Component {
     this.state = {
       roomCode: null,
     };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
 
   async componentDidMount() {
@@ -47,35 +48,22 @@ export default class HomePage extends Component {
       );
     }
   }
-//   renderHomePage() {
-//     return (
-//       <Grid container spacing={3}>
-//         <Grid item xs={12} align="center">
-//           <Typography variant="h3" compact="h3">
-//             House Party
-//           </Typography>
-//         </Grid>
-//         <Grid item xs={12} align="center">
-//           <ButtonGroup disableElevation variant="contained" color="primary">
-//             <Button color="primary" to="/join" component={Link}>
-//               Join a Room
-//             </Button>
-//             <Button color="secondary" to="/create" component={Link}>
-//               Create a Room
-//             </Button>
-//           </ButtonGroup>
-//         </Grid>
-//       </Grid>
-//     );
-//   }
+  clearRoomCode() {
+    this.setState({
+        roomCode: null,
+    })
+  }
   render() {
     return (
       <BrowserRouter>
-        <Routes> 
+        <Routes>
           <Route path="/" element={this.renderHomePage()} />
           <Route path="/join" element={<RoomJoinPage />} />
           <Route path="/create" element={<CreateRoomPage />} />
-          <Route path="/room/:roomCode" element={<Room />} />
+          <Route
+            path="/room/:roomCode"
+            element={<Room clearRoomCodeCallback={this.clearRoomCode} />}
+          />
         </Routes>
       </BrowserRouter>
     );
--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -1,6 +1,6 @@
 import React, { Component, useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-
+import {  useParams, useNavigate } from "react-router-dom";
+import { Grid, Button, Typography } from "@mui/material";
 
 export default function Room(props) {
   const initialState = {
@@ -10,10 +10,18 @@ export default function Room(props) {
   };
   const [roomData, setRoomData] = useState(initialState);
   const { roomCode } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch("/api/get-room" + "?code=" + roomCode)
-      .then((res) => res.json())
+      .then((response) => {
+        if (!response.ok) {
+          props.clearRoomCodeCallback(); // clears roomCode state in HomePage
+          navigate("/");
+        } else {
+          return response.json();
+        }
+      })
       .then((data) => {
         setRoomData({
           ...roomData,
@@ -23,12 +31,73 @@ export default function Room(props) {
         });
       });
   }, [roomCode, setRoomData]); //It renders when the object changes .If we use roomData and/or roomCode then it rerenders infinite times
+
+//   React.useEffect(() => {
+//     fetch(`/api/get-room?code=${roomCode}`)
+//       .then((response) => {
+//         if (!response.ok) {
+//           props.clearRoomCodeCallback(); // clears roomCode state in HomePage
+//           navigate("/");
+//         } else {
+//           return response.json();
+//         }
+//       })
+//       .then((data) => {
+//         setState({
+//           votesToSkip: data.votes_to_skip,
+//           guestCanPause: data.guest_can_pause,
+//           isHost: data.is_host,
+//         });
+//       });
+//   }, []);
+
+  const leaveButtonPressed = () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/api/leave-room", requestOptions).then((_response) => {
+      props.clearRoomCodeCallback(); // clears roomCode state in HomePage
+      navigate("/");
+    });
+  };
   return (
-    <div>
-      <h3>{roomCode}</h3>
-      <p>Votes: {roomData.votesToSkip}</p>
-      <p>Guest: {roomData.guestCanPause.toString()}</p>
-      <p>Host: {roomData.isHost.toString()}</p>
-    </div>
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography variant="h6" component="h6">
+          Code: {roomCode}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Typography variant="h6" component="h6">
+          Votes: {roomData.votesToSkip}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Typography variant="h6" component="h6">
+          Guest: {roomData.guestCanPause.toString()}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Typography variant="h6" component="h6">
+          Host: {roomData.isHost.toString()}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={leaveButtonPressed}
+        >
+          Leave Room
+        </Button>
+      </Grid>
+    </Grid>
+    // <div>
+    //   <h3>{roomCode}</h3>
+    //   <p>Votes: {roomData.votesToSkip}</p>
+    //   <p>Guest: {roomData.guestCanPause.toString()}</p>
+    //   <p>Host: {roomData.isHost.toString()}</p>
+    // </div>
   );
 }
