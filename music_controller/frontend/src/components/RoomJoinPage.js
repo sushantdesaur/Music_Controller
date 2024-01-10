615aaeff46488855870396c4ffd8cea53b5dd0eb
sushantdesaur <sushantdesaur@live.in>
2024-01-10 18:26:26 +0530

    Minor edit

--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,8 +1,7 @@
 
 import React, { useState, Component } from "react";
-import { Link } from "react-router-dom";
 import { TextField, Button, Grid, Typography } from  "@mui/material";
-import {useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
 export default function RoomJoinPage(props) {
   const navigate = useNavigate();
